f7291bab9e05a2fd98b5c4b91a4f51815a62a56f
Ankur Baranwal <ankur.baranwal@cl-macmini-127.local>
2019-10-18 12:45:26 +0530

    Initial Commit

---
+++ TabBarPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabBarPractice
+//
+//  Created by Ankur Baranwal on 18/10/2019.
+//  Copyright © 2019 Ankur Baranwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
